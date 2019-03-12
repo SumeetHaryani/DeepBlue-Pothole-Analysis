7c16eb58ad925376dba5a6e894aea9a1c95b568c
SumeetHaryani <sumeetharyani3@gmail.com>
2019-03-13 00:40:17 +0530

    changing port no

--- app.js
+++ app.js
@@ -83,6 +83,6 @@ app.use("/", authRoutes);
 // );
 
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT,process.env.IP, function () {
 	console.log("Server has started at PORT 3000");
 });
